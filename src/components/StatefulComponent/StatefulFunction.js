c17800e643c4cc98dbbd08e3b63173b96308fc96
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-03-23 21:37:46 +0400

    useEffect hook added

--- src/components/StatefulComponent/StatefulFunction.js
+++ src/components/StatefulComponent/StatefulFunction.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 
 const Clock4 = (props) => {
-    const [clock, setClock] = useState({ date: new Date() })
-    setInterval(() => {
-        setClock({ date: new Date() });
-    }, 1000)
+    const [clock, setClock] = useState(new Date())
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setClock(new Date())
+        }, 1000)
+        //this returns a function which stop the rendering(componentWillUnmount)
+        return () => clearInterval(interval);
+    }, []);//this dependency will act like componentDidMount and componentWillUpdate
+    //empty array means no dependency;so the component just mount(render in the dom) one time as like as componentDidMount.when the array is fullfiled with dependencies; that will act like componentWillUpdate.
     return (
         <div>
             <h1>
-                <span>Hello {clock.date.toLocaleTimeString(props.locale)}</span>
+                <span>Hello {clock.toLocaleTimeString(props.locale)}</span>
             </h1>
         </div>
     );
--- src/components2/StatefulComponent/Function.js
+++ src/components2/StatefulComponent/Function.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Clock8 = () => {
-    const [clock, setClock] = useState({ date: new Date() });
-    setInterval(() => {
-        setClock({ date: new Date() });
-    }, 1000)
+    const [clock, setClock] = useState(new Date());
+    useEffect(() => {
+        const interval = setInterval(() => setClock(new Date()), 1000)
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div>
             <h1>
-                <span>Hello {clock.date.toLocaleTimeString()}</span>
+                <span>Hello {clock.toLocaleTimeString()}</span>
             </h1>
         </div>
     )
 }
 
 export default Clock8;
+
